00cc1c76cd02012e2b1d9e8592edc1eacd196a99
nurulamin092 <mnurulamin092@gmial.com>
2022-05-25 22:18:22 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${[process.env.DB_PASS]}@cluster0.5vk1e.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('MongoDb  connected');
+
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
-    res.send('Bismillah')
+    res.send('Hello Bicycle manufacture!')
 })
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Bicycle Manufacture App listening on port ${port}`)
 })
