792c474c448e704a3a5052d5b264c4f3baa9c976
nurulamin092 <mnurulamin092@gmial.com>
2022-05-28 00:22:39 +0600

    user amin role save in mongodb

--- index.js
+++ index.js
@@ -46,6 +46,15 @@ async function run() {
             res.send(products)
         });
 
+        app.put('/user/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
         app.put('/user/:email', async (req, res) => {
             const email = req.params.email;
             const user = req.body;
